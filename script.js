73f3eab83fcfbd017ce9407956d85d59959c14aa
Naty <natyaith@yahoo.com.br>
2023-02-04 18:26:53 -0300

    add questões

---
+++ script.js
@@ -0,0 +1,48 @@
+ const quizData = [
+    {
+        question:"Qual dessas frutas os cães não podem comer?",
+        a:"Banana",
+        b:"Uva",
+        c:"Melancia",
+        d:"Manga",
+        correct:"b",
+    },
+
+    {
+        question:"Qual é considerada a menor raça do mundo?",
+        a:"Chihuahua",
+        b:"Yorkshire",
+        c:"ShihTzu",
+        d:"Spitz",
+        correct:"a",
+    },
+
+    {
+        question:"Qual o tempo médio de gestação de uma cachorrinha?",
+        a:"9 meses",
+        b:"1 mês",
+        c:"2 meses",
+        d:"4 meses",
+        correct:"c",
+    },
+
+    {
+        question:"Qual a raça do Scooby Doo?",
+        a:"Poodle gigante",
+        b:"Boxer",
+        c:"Dogue Alemão",
+        d:"Pastor Suíço",
+        correct:"c",
+    },
+
+    {
+        question:"Qual é considerada a raça mais inteligente?",
+        a:"Pug",
+        b:"Buldogue Inglês",
+        c:"Pequinês",
+        d:"Border Collie",
+        correct:"d",
+    },
+
+];
+
